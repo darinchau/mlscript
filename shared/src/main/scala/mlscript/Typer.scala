ac8a8f381024b86336a43d6f112608357de2313f
Darin Chau <darinchauyf@gmail.com>
2024-04-02 06:18:16 +0800

    Remove declared types for now

--- shared/src/main/scala/mlscript/Typer.scala
+++ shared/src/main/scala/mlscript/Typer.scala
@@ -239,14 +239,15 @@ class Typer(var dbg: Boolean, var verbose: Bool, var explainErrors: Bool, val ne
     NuTypeDef(Cls, TN("Str"), Nil, N, N, N, Nil, N, N, TypingUnit(Nil))(N, S(preludeLoc)),
     NuTypeDef(Als, TN("undefined"), Nil, N, N, S(Literal(UnitLit(true))), Nil, N, N, TypingUnit(Nil))(N, S(preludeLoc)),
     NuTypeDef(Als, TN("null"), Nil, N, N, S(Literal(UnitLit(false))), Nil, N, N, TypingUnit(Nil))(N, S(preludeLoc)),
-    // For unboxed options:
-    // Note to self: this exists so desugarer wont screw things up and we get nice case branches for options
-    NuTypeDef(Cls, TN("Some"), (S(VarianceInfo.co) -> TN("A")) :: Nil,
-      S(Tup( (S(Var("value")), Fld(FldFlags.empty, Var("A"))) :: Nil )),
-      N, N, Nil, N, N,
-      TypingUnit(Nil))(N, N),
-    NuTypeDef(Mod, TN("None"), Nil, N, N, N, Nil, N, N, TypingUnit(Nil))(N, N),
-    // 
+    // // For unboxed options:
+    // // Note to self: this exists so desugarer wont screw things up and we get nice case branches for options
+    // NuTypeDef(Cls, TN("Option"), (S(VarianceInfo.co) -> TN("A")) :: Nil, N, N, N, Nil, N, N, TypingUnit(Nil))(N, N),
+    // NuTypeDef(Cls, TN("Some"), (S(VarianceInfo.co) -> TN("A")) :: Nil,
+    //   S(Tup( (S(Var("value")), Fld(FldFlags.empty, Var("A"))) :: Nil )),
+    //   N, N, Nil, N, N,
+    //   TypingUnit(Nil))(N, N),
+    // NuTypeDef(Mod, TN("None"), Nil, N, N, N, TyApp(Var("Option"),TN("nothing") :: Nil) :: Nil, N, N, TypingUnit(Nil))(N, N),
+    // // Well this "exists" but is not usable as an object. Wow
   )
   val builtinTypes: Ls[TypeDef] =
     TypeDef(Cls, TN("?"), Nil, TopType, Nil, Nil, Set.empty, N, Nil) :: // * Dummy for pretty-printing unknown type locations
